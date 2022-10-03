297ddb750dc05814a8c5935f9703080aa67ea640
Dorian Dubreus <doriandubreus@gmail.com>
2022-10-03 19:15:39 -0400

    added the route to get feed and dashboard

--- routes/main.js
+++ routes/main.js
@@ -8,9 +8,10 @@ const postController = require('../controllers/posts')
 router.get("/", homeController.getIndex);
 router.post('/login', authController.postLogin) //for login authentication
 router.get('/login', authController.getLogin)
+router.get('/feed', ensureAuth, postController.getFeed)
 router.get('/dashboard', ensureAuth, postController.getDashboard) //where we left off: make a dashboard view so we can finish the sign up 
 router.get('/signup', authController.getSignup)
 router.post('/signup', authController.postSignup)
-
+router.get('/logout', authController.logout)
 
 module.exports = router
