84861bf2130dd0c53343653773a30e02eb483e5d
Dorian Dubreus <doriandubreus@gmail.com>
2022-10-03 19:14:29 -0400

    routing for creating a post, getting the addPost, getting the feed and getting the individual post

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,6 +2,60 @@ const Post = require('../models/Post')
 
 module.exports = {
     getDashboard: async (req, res) => {
-        res.render('dashboard.ejs')
+        try {
+            const post = await Post.find({ user: req.user.id })
+            res.render('dashboard.ejs', {
+                posts: post,
+                user: req.user
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    },
+
+    getAddPost: async (req, res) => {
+        res.render('addPost.ejs')
+    },
+
+
+    createPost: async (req, res) => {
+        try {
+            await Post.create({
+                title: req.body.title,
+                body: req.body.body,
+                status: req.body.status,
+                likes: 0,
+                user: req.user.id,
+
+            });
+            console.log('post has been added')
+            res.redirect('/dashboard')
+
+        } catch (err) {
+            console.log(err)
+        }
+    },
+
+    getFeed: async (req, res) => {
+        try {
+            const posts = await Post.find().sort({ createdAt: "desc" }).lean()
+            res.render('feed.ejs', {
+                posts: posts
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    },
+
+    getPost: async (req, res) => {
+        try {
+            const post = await Post.findById(req.params.id)
+            res.render('post.ejs', {
+                post: post,
+                user: req.user
+            })
+        } catch (err) {
+            console.log(err)
+        }
     }
 }
